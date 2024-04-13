c329bb183c5daaa633e60f87c64153219696658f
Marius Storhaug <marstor@hotmail.com>
2024-04-14 00:04:33 +0200

    remove module import test

--- tests/NerdFonts.Tests.ps1
+++ tests/NerdFonts.Tests.ps1
@@ -8,16 +8,6 @@ Param(
 Write-Verbose "Path to the module: [$Path]" -Verbose
 
 Describe 'Module' {
-    Context 'NerdFonts' {
-        It 'The module should be available' {
-            Get-Module -Name 'NerdFonts' -ListAvailable | Should -Not -BeNullOrEmpty
-            Write-Verbose (Get-Module -Name 'NerdFonts' -ListAvailable | Out-String) -Verbose
-        }
-        It 'The module should be imported' {
-            { Import-Module -Name 'NerdFonts' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
-        }
-    }
-
     Context 'Function: Get-NerdFont' {
         It 'Function exists' {
             Get-Command Get-NerdFont | Should -Not -BeNullOrEmpty
