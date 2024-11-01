6dfe590bfa553ce3fe6abd154cbc54e2fc66cc6d
Marius Storhaug <marstor@hotmail.com>
2024-11-02 00:43:15 +0100

    🩹 [Patch]: Remove redundant parameter from tests (#19)
    ## Description
    
    - Remove redundant parameter from tests
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/NerdFonts.Tests.ps1
+++ tests/NerdFonts.Tests.ps1
@@ -1,13 +1,4 @@
-﻿[CmdletBinding()]
-Param(
-    # Path to the module to test.
-    [Parameter()]
-    [string] $Path
-)
-
-Write-Verbose "Path to the module: [$Path]" -Verbose
-
-Describe 'Module' {
+﻿Describe 'Module' {
     Context 'Function: Get-NerdFont' {
         It 'Function exists' {
             Get-Command Get-NerdFont | Should -Not -BeNullOrEmpty
