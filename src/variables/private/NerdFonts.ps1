9ef19429f9dbfd85ace7a963d6938688832d2494
Marius Storhaug <marstor@hotmail.com>
2025-01-12 21:24:20 +0100

    Move NerdFonts initialization to use private variables

---
+++ src/variables/private/NerdFonts.ps1
@@ -0,0 +1,9 @@
+﻿$script:Release = Invoke-RestMethod 'https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest'
+$script:NerdFonts = $script:Release.assets.browser_download_url | Where-Object { $_ -like '*.zip' } | ForEach-Object {
+    [pscustomobject]@{
+        Name    = $_.Split('/')[-1].Split('.')[0]
+        Version = $script:Release.tag_name
+        URL     = $_
+    }
+}
+$script:ArchiveExtension = '.zip'
