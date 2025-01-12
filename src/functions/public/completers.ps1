18663ecff76481a98f4f0e322a852a9a2cb4d3c0
Marius Storhaug <marstor@hotmail.com>
2025-01-12 22:32:59 +0100

    Fix completer

--- src/functions/public/completers.ps1
+++ src/functions/public/completers.ps1
@@ -2,7 +2,7 @@
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
 
-    Get-NerdFont | Select-Object -ExpandProperty Name | Where-Object { $_ -like $wordToComplete } | ForEach-Object {
+    Get-NerdFont | Select-Object -ExpandProperty Name | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
         [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
 }
