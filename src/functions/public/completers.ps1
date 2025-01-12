6cf3073cfd316bb8a3625025d22adaa7b892942c
Marius Storhaug <marstor@hotmail.com>
2025-01-12 21:41:23 +0100

    Refactor NerdFont completion logic to simplify property access

--- src/functions/public/completers.ps1
+++ src/functions/public/completers.ps1
@@ -2,7 +2,7 @@
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
 
-    Get-NerdFont | Where-Object { $_.Name -like $wordToComplete } | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name)
+    Get-NerdFont | Select-Object -ExpandProperty Name | Where-Object { $_ -like $wordToComplete } | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
 }
